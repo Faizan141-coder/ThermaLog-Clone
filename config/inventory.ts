ab1652ce86cede011446e1b17aaca9591b24e8eb
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-06 15:37:43 +0500

    Update inventory.ts

--- config/inventory.ts
+++ config/inventory.ts
@@ -28,6 +28,46 @@ export const inventory: InventoryProduct[] = [
     id: "64da6006-a4bb-4555-af78-3467853eb75e",
     sku: "canvas_tote_bag_1",
     name: "Canvas Tote Bag",
+    description: '“Sense ball” is small, battery powered device that can provide you with storage monitoring features like inside temperature, humidity, light intensity and gas emissions beside GPS tracker to monitor real time supply chain. ',
+    price: 16800, 
+    image:
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
+    images: [
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-2.jpg",
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-3.jpg",
+    ],
+    features: ["Acoustic Signatures Detection"],
+    sensingRanges: ["bags"],
+    applications: ["brown"],
+    description1: 'asasas',
+    description2: 'asasasasasasas',
+    currency: "USD",
+  },
+  {
+    id: "392167ff-2199-4211-b5d4-68a47729c1f9",
+    sku: "ssscanvas_tote_bag_1",
+    name: "sss Tote Bag",
+    description: `Meet your new favorite carry-on. Our supersized tote is crafted in durable waxed cotton canvas that's rugged and durable, ideal for hauling all of your stuff. This size takes you to and from the farmer's market, the gym or a weekend getaway.`,
+    price: 16800, // price in smallest currency unit (e.g. cent for USD)
+    image:
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
+    images: [
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-2.jpg",
+      "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-3.jpg",
+    ],
+    features: ["one-size"],
+    sensingRanges: ["bags"],
+    applications: ["brown"],
+    description1: 'asasas',
+    description2: 'asasasasasasas',
+    currency: "USD",
+  },
+  {
+    id: "392167gf-2199-4211-b5d4-68a47729c1f9",
+    sku: "d",
+    name: "d Tote Bag",
     description: `Meet your new favorite carry-on. Our supersized tote is crafted in durable waxed cotton canvas that's rugged and durable, ideal for hauling all of your stuff. This size takes you to and from the farmer's market, the gym or a weekend getaway.`,
     price: 16800, // price in smallest currency unit (e.g. cent for USD)
     image:
