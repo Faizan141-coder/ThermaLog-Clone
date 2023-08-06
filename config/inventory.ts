1cbeca9be24c9a2be57e4dff8eea71e40660fb68
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-06 20:34:31 +0500

    commit

--- config/inventory.ts
+++ config/inventory.ts
@@ -26,10 +26,10 @@ export interface SanityProduct extends Omit<InventoryProduct, "images"> {
 export const inventory: InventoryProduct[] = [
   {
     id: "64da6006-a4bb-4555-af78-3467853eb75e",
-    sku: "canvas_tote_bag_1",
-    name: "Canvas Tote Bag",
-    description: '“Sense ball” is small, battery powered device that can provide you with storage monitoring features like inside temperature, humidity, light intensity and gas emissions beside GPS tracker to monitor real time supply chain. ',
-    price: 16800, 
+    sku: "smart_energy_monitoring_device_1",
+    name: "Smart Energy Monitoring Device",
+    description: 'Our Smart Energy Meters are low-cost, non - invasive / invasive , indigenous designed plug and play IoT-based three phase smart AC energy meters. Smart AC Energy Meter captures 12 essential parameters of AC Power for each phase and transmits it wirelessly to the central cloud.',
+    price: 46550, 
     image:
       "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
     images: [
@@ -46,10 +46,10 @@ export const inventory: InventoryProduct[] = [
   },
   {
     id: "392167ff-2199-4211-b5d4-68a47729c1f9",
-    sku: "ssscanvas_tote_bag_1",
-    name: "sss Tote Bag",
-    description: `Meet your new favorite carry-on. Our supersized tote is crafted in durable waxed cotton canvas that's rugged and durable, ideal for hauling all of your stuff. This size takes you to and from the farmer's market, the gym or a weekend getaway.`,
-    price: 16800, // price in smallest currency unit (e.g. cent for USD)
+    sku: "agri_farm_multi_meter_1",
+    name: "Agri Farm Multi Meter",
+    description: `In agriculture, timely usage of water maintains the health of crops. Delayed Irrigation leads to loss of nutrition of crops and overly? early irrigation saturates land which leads to various fungal and algae disease attacks on crops and plants`,
+    price: 46800, // price in smallest currency unit (e.g. cent for USD)
     image:
       "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
     images: [
@@ -66,10 +66,10 @@ export const inventory: InventoryProduct[] = [
   },
   {
     id: "392167gf-2199-4211-b5d4-68a47729c1f9",
-    sku: "d",
-    name: "d Tote Bag",
-    description: `Meet your new favorite carry-on. Our supersized tote is crafted in durable waxed cotton canvas that's rugged and durable, ideal for hauling all of your stuff. This size takes you to and from the farmer's market, the gym or a weekend getaway.`,
-    price: 16800, // price in smallest currency unit (e.g. cent for USD)
+    sku: "sense_ball_1",
+    name: "Sense Ball",
+    description: `“Sense ball” is small, battery powered device that can provide you with storage monitoring features like inside temperature, humidity, light intensity and gas emissions beside GPS tracker to monitor real time supply chain. `,
+    price: 36800, // price in smallest currency unit (e.g. cent for USD)
     image:
       "https://fullstackdotso.nyc3.cdn.digitaloceanspaces.com/canvas-tote-bag-1.jpg",
     images: [
