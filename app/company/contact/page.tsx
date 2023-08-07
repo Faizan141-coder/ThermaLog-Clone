c6aaa77fc34eb743a008a858967991c959911548
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-07 11:32:17 +0500

    Update page.tsx

--- app/company/contact/page.tsx
+++ app/company/contact/page.tsx
@@ -9,11 +9,11 @@ const page: React.FC = () => {
         {/* <Navbar /> */}
           <div className='my-10 flex px-52'>
             <div className='text-justify'>
-                <div className='flex'>
+                {/* <div className='flex'>
                     <div className='flex-wrap'>
                         <h1 className='text-[40px] font-semibold'>Contact<span className='text-blue-400'> Us</span></h1>
                     </div>
-                </div>
+                </div> */}
                 {/* <div>
                     <p className='text-md mt-5'>
                         This is will the contact form
