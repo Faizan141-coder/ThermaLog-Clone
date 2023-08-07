fb0a20804a877da8bc97f04d81dbfcc4448de77e
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-07 12:09:02 +0500

    commit

--- app/company/team/page.tsx
+++ app/company/team/page.tsx
@@ -14,25 +14,20 @@ const Page: React.FC = () => {
         </div>
         <h1 className='mt-10 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Board</span></h1>
         <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <Image src='/persons/8.jpg' alt='VGreen' width={150} height={30} className="rounded-md" />
-            <Image src='/persons/9.jpg' alt='National Incubation Center' width={150} height={30} className="rounded-md" />
-            <Image src='/persons/10.jpg' alt='PFAN' width={150} height={30} className="rounded-md" />
-        </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Tech & Data</span></h1>
-        <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <Image src='/persons/4.jpg' alt='VGreen' width={150} height={30} className="rounded-md" />
-            <Image src='/persons/5.jpg' alt='National Incubation Center' width={150} height={30} className="rounded-md" />
-        </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Product</span></h1>
-        <div className="my-5 flex flex-wrap items-start space-x-10">
-            <Image src='/persons/1.jpg' alt='VGreen' width={150} height={30} className="mb-2 rounded-md" />
+            <Image src='/persons/1.jpeg' alt='VGreen' width={150} height={30} className="rounded-md" />
             <Image src='/persons/2.jpg' alt='National Incubation Center' width={150} height={30} className="rounded-md" />
-            <Image src='/persons/3.jpg' alt='PFAN' width={150} height={30} className="rounded-md" />
         </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Sales & Marketinng</span></h1>
+        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Design Engineer</span></h1>
         <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <Image src='/persons/6.jpg' alt='VGreen' width={150} height={30} className="rounded-lg" />
-            <Image src='/persons/7.jpg' alt='National Incubation Center' width={150} height={30} className="rounded-lg" />
+            <Image src='/persons/3.jpeg' alt='VGreen' width={150} height={30} className="rounded-md" />
+        </div>
+        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Product Designer</span></h1>
+        <div className="my-5 flex flex-wrap items-start space-x-10">
+            <Image src='/persons/4.png' alt='VGreen' width={150} height={30} className="mb-2 rounded-md" />
+        </div>
+        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Media Manager</span></h1>
+        <div className="mt-5 flex flex-wrap items-start space-x-10">
+            <Image src='/persons/5.jpg' alt='VGreen' width={150} height={30} className="rounded-lg" />
         </div>
       </div>
       
