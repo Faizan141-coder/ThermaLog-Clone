267a187dc5c608b9beeab8046a887b96ddbe8523
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-07 13:22:45 +0500

    commit

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -67,16 +67,7 @@ export default async function Page({ searchParams }: Props) {
           </div>
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
-            {/* <h2 id="products-heading" className="sr-only">
-              Products
-            </h2> */}
-            {/* <div className={cn("grid grid-cols-1 gap-x-8 gap-y-10", products.length > 0 ? 'lg:grid-cols-4' : 'lg:grid-cols-[1fr_3fr]')}> */}
-              {/* <div className="hidden lg:block">
-                <ProductFilters />
-              </div> */}
-              
-              <ProductGrid products={products}/>              
-            {/* </div> */}
+            <ProductGrid products={products}/>              
           </section>
         </main>
       </div>
