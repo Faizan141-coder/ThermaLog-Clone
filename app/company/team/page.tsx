e47ff65ae85229fad4b256ba8c320d0173b5c211
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-08-08 16:08:05 +0500

    commit

--- app/company/team/page.tsx
+++ app/company/team/page.tsx
@@ -5,63 +5,90 @@ const Page: React.FC = () => {
   return (
     <div>
       <div className="my-10 px-52">
+
         <div className="text-justify">
             <div className="flex-wrap">
                 <h1 className="text-[44px] font-semibold">
-                Meet Our <span className="text-blue-400">Team</span>
+                  Meet Our <span className="text-blue-400">Team</span>
                 </h1>
             </div>
         </div>
+
         <h1 className='mt-10 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Board</span></h1>
         <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <div>
-            <Image src='/persons/1.jpeg' alt='Dr. Shehzad Younas' width={150} height={30} className="rounded-md" />
-            <p className="text-center">Dr. Shehzad Younas</p>
+            <div className="flex flex-col items-center justify-center">
+              <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+                <img src="/persons/2222.png" alt="Dr. Shehzad Younas" width="150" height="150" />
+              </div>
+              <p className="mb-1 text-center text-xl font-bold">Dr. Shehzad Younas</p>
+              <p className="text-center">(Product Development Engineer)</p>
             </div>
-            <div>
-              <Image src='/persons/22.jpg' alt='Muhammad Kashif' width={150} height={30} className="rounded-md" />
-              <p className="text-center">Muhammad Kashif</p>
+            <div className="flex flex-col items-center justify-center">
+              <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+                <img src="/persons/22.jpg" alt="Muhammad Kashif" width="150" height="150" />
+              </div>
+              <p className="mb-1 text-center text-xl font-bold">Muhammad Kashif</p>
+              <p className="text-center">(Product Development Engineer)</p>
             </div>
         </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Design Engineer</span></h1>
+
+        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Product</span></h1>
         <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <div>
-              <Image src='/persons/3.jpeg' alt='Muhammad Tayyab' width={150} height={30} className="rounded-md" />
-              <p className="text-center">Muhammad Tayyab</p>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/aa.png" alt="Muhammad Tayyab" width="150" height="150" />
             </div>
-            <div>
-              <Image src='/persons/Muhammad-Uzair.jpeg' alt='Muhammad Uzair' width={150} height={30} className="rounded-md" />
-              <p className="text-center">Muhammad Uzair</p>
+            <p className="mb-1 text-center text-xl font-bold">Muhammad Tayyab</p>
+            <p className="text-center">(Design Engineer)</p>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/Muhammad-Uzair.jpeg" alt="Muhammad Uzair" width="150" height="150" />
             </div>
+            <p className="mb-1 text-center text-xl font-bold">Muhammad Uzair</p>
+            <p className="text-center">(Design Engineer)</p>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/4.png" alt="Naveen Akbar" width="150" height="150" />
+            </div>
+            <p className="mb-1 text-center text-xl font-bold">Naveen Akbar</p>
+            <p className="text-center">(Product Designer)</p>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/8.jpeg" alt="Nazish Zulfiqar" width="150" height="150" />
+            </div>
+            <p className="mb-1 text-center text-xl font-bold">Nazish Zulfiqar</p>
+            <p className="text-center">(Senior Research Scientist)</p>
+          </div>
         </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Product Designer</span></h1>
-        <div className="my-5 flex flex-wrap items-start space-x-10">
-            <div>
-              <Image src='/persons/4.png' alt='Naveen Akbar' width={150} height={30} className="mb-2 rounded-md" />
-              <p className="text-center">Naveen Akbar</p>
-            </div>
-        </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Media Manager</span></h1>
+
+        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Sales & Marketing</span></h1>
         <div className="mt-5 flex flex-wrap items-start space-x-10">
-            <div>
-              <Image src='/persons/55.jpg' alt='Nida Nabeel' width={150} height={30} className="rounded-lg" />
-              <p className="text-center">Nida Nabeel</p>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/a.png" alt="Nida Nabeel" width="150" height="150" />
             </div>
-        </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Team Members</span></h1>
-        <div className="my-5 flex flex-wrap items-start space-x-10">
-            <div>
-              <Image src='/persons/Kashif-Janjua.jpeg' alt='Kashif Janjua' width={150} height={30} className="mb-2 rounded-md" />
-              <p className="text-center">Kashif Janjua</p>
+            <p className="mb-1 text-center text-xl font-bold">Nida Nabeel</p>
+            <p className="text-center">(Media Manager)</p>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center">
+            <div className="mx-auto mb-2 h-36 w-36 overflow-hidden rounded-full">
+              <img src="/persons/Kashif-Janjua.jpeg" alt="Kashif Janjua" width="150" height="150" />
             </div>
+            <p className="mb-1 text-center text-xl font-bold">Kashif Janjua</p>
+            <p className="text-center">(Team Member)</p>
+          </div>
+
         </div>
-        <h1 className='mt-5 px-24 pt-5 text-center text-4xl font-bold'><span className="text-blue-400">Senior Research Scientist</span></h1>
-        <div className="my-5 flex flex-wrap items-start space-x-10">
-            <div>
-              <Image src='/persons/8.jpeg' alt='Nazish Zulfiqar' width={150} height={30} className="mb-2 rounded-md" />
-              <p className="text-center">Nazish Zulfiqar</p>
-            </div>
-        </div>
+
       </div>
       
     </div>
